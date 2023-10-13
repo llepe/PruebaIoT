d4c1f266d23865bf72e9976ce0b948ef01f2eba1
Luis Lepe <luismanuel.lepe@gmail.com>
2023-10-13 17:46:59 -0600

    Revisado si es windows o RBPi y ejecutar el codigo correspondiente.

--- LuisManuelLepeVizcaino/PruebaLEDUniversal.py
+++ LuisManuelLepeVizcaino/PruebaLEDUniversal.py
@@ -1,20 +1,24 @@
-from tkgpio import TkCircuit
+from pathlib import Path
+is_rpi = Path("/etc/rpi-issue").exists()
 
-# initialize the circuit inside the GUI
+if not is_rpi:
+    from tkgpio import TkCircuit
 
-configuration = {
-    "width": 300,
-    "height": 200,
-    "leds": [
-        {"x": 50, "y": 40, "name": "LED 1", "pin": 15},
-    ],
-    "buttons": [
-        {"x": 50, "y": 130, "name": "Press to toggle LED 2", "pin": 11},
-    ]
-}
+    # initialize the circuit inside the GUI
 
-circuit = TkCircuit(configuration)
-@circuit.run
+    configuration = {
+        "width": 300,
+        "height": 200,
+        "leds": [
+            {"x": 50, "y": 40, "name": "LED 1", "pin": 15},
+        ],
+        "buttons": [
+            {"x": 50, "y": 130, "name": "Press to toggle LED 2", "pin": 11},
+        ]
+    }
+
+    circuit = TkCircuit(configuration)
+#@circuit.run
 
 def main ():
     
@@ -29,3 +33,9 @@ def main ():
    
     while True:
         sleep(0.1)
+
+if is_rpi:
+    main()
+else:
+    circuit.run(main)
+
